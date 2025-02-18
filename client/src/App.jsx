253b43d2b58d2584cde65462db897c002dfb931b
sourav <mailsourav014@gmail.com>
2025-02-18 23:29:17 +0530

    implemented signin and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Landing from "./components/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
