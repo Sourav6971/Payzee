3f1144c44601c20a2a68b48285829db86051c916
sourav <mailsourav014@gmail.com>
2025-02-22 18:19:29 +0530

    added custom hooks

---
+++ client/src/hooks/UseDebounce.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export function useDebounce(inputValue) {
+  const [debouncedValue, setDebouncedValue] = useState(inputValue);
+  useEffect(() => {
+    const value = setTimeout(() => {
+      setDebouncedValue(inputValue);
+    }, 200);
+    return () => {
+      clearTimeout(value);
+    };
+  }, [inputValue]);
+  return debouncedValue;
+}
