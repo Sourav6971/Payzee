35a3215b8c2ef40589a8f7043d3d1aa9a4f79cd0
sourav <mailsourav014@gmail.com>
2025-02-25 13:41:55 +0530

    added modals for account creation

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>I am footer</div>;
+  return (
+    <footer className="w-full bg-black text-white  flex justify-center items-center space-x-6 py-10">
+      <a
+        href="https://github.com/Sourav6971"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center space-x-2 hover:text-gray-400 transition"
+      >
+        <FaGithub size={20} />
+        <span>@Sourav6971</span>
+      </a>
+      <a
+        href="https://github.com/Sourav6971/payzee"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center space-x-2 hover:text-gray-400 transition"
+      >
+        <FaGithub size={20} />
+        <span>Create a fork</span>
+      </a>
+      <span className="text-sm">
+        &copy; {"Copyright " + new Date().getFullYear()}
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ client/src/components/PasswordModal.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const PasswordModal = ({ isOpen, onClose, onConfirm }) => {
+  const [password, setPassword] = useState("");
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md z-50">
+      <div className="bg-white/10 border border-white/20 p-6 rounded-xl shadow-xl w-96 backdrop-blur-lg text-white">
+        <h2 className="text-2xl font-semibold mb-4 text-center">
+          Enter Password
+        </h2>
+        <p className="text-gray-300 text-center mb-4">
+          Please enter your password to proceed.
+        </p>
+
+        <input
+          type="password"
+          className="input input-bordered w-full bg-white/10 text-white placeholder-gray-300 backdrop-blur-md p-2 rounded-sm "
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div className="flex flex-col space-y-3 mt-6">
+          <button
+            onClick={() => onConfirm(password)}
+            className="btn btn-primary btn-block bg-blue-600 hover:bg-blue-700 glass rounded-sm p-2"
+          >
+            Confirm
+          </button>
+        </div>
+
+        <div className="flex justify-center mt-4">
+          <button
+            onClick={onClose}
+            className="btn btn-outline btn-sm text-gray-300 hover:text-white"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordModal;
