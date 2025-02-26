8c04be69876e355ecf8469c296af4042fe9f690a
sourav <mailsourav014@gmail.com>
2025-02-26 19:32:33 +0530

    implemented signin loader

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -2,10 +2,13 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import Loader from "./Loader";
 
 const SignIn = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const [inputForm, setInputForm] = useState({
     username: "",
     password: "",
@@ -16,6 +19,7 @@ const SignIn = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await axios.post(
         "https://payzee-taupe.vercel.app/api/user/signin",
@@ -34,6 +38,8 @@ const SignIn = () => {
       }
     } catch (err) {
       alert("Sign in failed");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -63,9 +69,13 @@ const SignIn = () => {
           </span>
         </div>
 
-        <button type="submit" className="submit-btn">
-          Sign In
-        </button>
+        {loading ? (
+          <Loader />
+        ) : (
+          <button type="submit" className="submit-btn">
+            Sign In
+          </button>
+        )}
       </form>
     </div>
   );
