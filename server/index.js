b3a50fcd0f286e6bd3c3d347c83aaf432fe9ed5f
sourav <mailsourav014@gmail.com>
2025-02-26 18:50:44 +0530

    added vercel

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.json({
+    msg: "app running successfully",
+  });
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/account", accountRoute);
