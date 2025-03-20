d8ff29db32600feaba6a6007fe5e535f6ccc4244
sourav <mailsourav014@gmail.com>
2025-03-20 21:35:01 +0530

    ui improvements

--- client/src/pages/Tutorials.jsx
+++ client/src/pages/Tutorials.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 const Tutorials = () => {
   return (
-    <div className="min-h-screen bg-background-dark text-text-light pt-20">
+    <div className="min-h-screen bg-background-dark text-text-light ">
       <Navbar />
       <div className="container mx-auto p-4">
         <div className="bg-background-light p-6 rounded-lg shadow-lg ">
