a8ceb9b59cb0672762b2bdc2f8255a18d408c217
sourav <mailsourav014@gmail.com>
2025-03-27 12:24:47 +0530

    payments

--- client/src/pages/Payments.jsx
+++ client/src/pages/Payments.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import axios from "axios";
+import BACKEND_URL from "../config";
+import { useDebounce } from "./../hooks/UseDebounce";
 
 const Payments = () => {
   const [availableUsers, setAvailableUsers] = useState([]);
+  const [userFilter, setUserFilter] = useState("");
+  const debouncedFilter = useDebounce(userFilter);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) navigate("/Auth");
+    axios
+      .get(BACKEND_URL + "user/get-users?filter=" + debouncedFilter, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((res) => setAvailableUsers(res.data.users))
+      .catch((err) => console.log(err));
+  }, [debouncedFilter]); // Updated dependency to debouncedFilter
 
   return (
     <div className="min-h-screen bg-background-dark text-text-light ">
       <Navbar />
       <div className="flex justify-center">
         <form className="flex self-center justify-center bg-[#693382] py-4 px-10 mt-20 rounded-2xl  align-middle">
-          <input placeholder="Search users.." className="outline-none"></input>
+          <input
+            placeholder="Search users.."
+            className="outline-none"
+            onChange={(e) => setUserFilter(e.target.value)}
+          ></input>
           <button type="submit" className="cursor-pointer">
             Search
           </button>
