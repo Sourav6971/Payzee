247216acdbdaad9dd9317b13a2dc695af7020bb5
Sourav6971 <mailsourav014@gmail.com>
2025-08-17 16:55:36 +0530

    phantom wallet integration started

--- client/src/components/ui/Index.jsx
+++ client/src/components/ui/Index.jsx
@@ -1,3 +1,10 @@
+import { FaPaperPlane } from "react-icons/fa";
+import { IoMdSwap } from "react-icons/io";
+import { FaCircleDollarToSlot } from "react-icons/fa6";
+import { IoQrCode } from "react-icons/io5";
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
+
 function Input({
   value,
   onChange,
@@ -17,5 +24,74 @@ function Input({
     />
   );
 }
+function SideBar() {
+  return (
+    <div className="h-[500px] max-w-[80px] bg-white shadow shadow-slate-300 mx-8 my-36 rounded-xl">
+      <div className="flex flex-col justify-around h-full py-6">
+        <span className="flex justify-center cursor-pointer">
+          <Tooltip
+            title="Send"
+            arrow
+            placement="right"
+            slots={{
+              transition: Zoom,
+            }}
+            slotProps={{
+              transition: { timeout: 100 },
+            }}
+          >
+            <FaPaperPlane color="#0047AB" size={30} />
+          </Tooltip>
+        </span>
+        <span className="flex justify-center cursor-pointer">
+          <Tooltip
+            title="Swap"
+            arrow
+            placement="right"
+            slots={{
+              transition: Zoom,
+            }}
+            slotProps={{
+              transition: { timeout: 100 },
+            }}
+          >
+            <IoMdSwap color="#0047AB" size={30} />
+          </Tooltip>
+        </span>
+        <span className="flex justify-center cursor-pointer">
+          <Tooltip
+            title="Buy"
+            arrow
+            placement="right"
+            slots={{
+              transition: Zoom,
+            }}
+            slotProps={{
+              transition: { timeout: 100 },
+            }}
+          >
+            <FaCircleDollarToSlot color="#0047AB" size={30} />
+          </Tooltip>
+        </span>
 
-export { Input };
+        <span className="flex justify-center cursor-pointer">
+          <Tooltip
+            title="Recieve"
+            arrow
+            placement="right"
+            slots={{
+              transition: Zoom,
+            }}
+            slotProps={{
+              transition: { timeout: 100 },
+            }}
+          >
+            <IoQrCode color="#0047AB" size={30} />
+          </Tooltip>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export { Input, SideBar };
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,31 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import { UserContext } from "../context/user/context";
+import { SideBar } from "../components/ui/Index";
+import toast from "react-hot-toast";
 export default function Dashboard() {
+  const [connected, setConnected] = useState(false);
   const { setUser } = useContext(UserContext);
+  const connectWallet = async () => {
+    const response = await window?.solana?.connect();
+    if (response) {
+      toast.success("Connected");
+      setConnected(true);
+    } else toast.error("Could not find wallet");
+    console.log(response);
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <div className="min-h-screen">
         <div className="shadow">
           <div className="flex justify-between  p-6 max-w-[1800px] mx-auto">
-            <div className="text-blue-900 ">Dashboard</div>
-            <DashboardNav />
+            <div className="text-blue-900 flex flex-col justify-center">
+              <text className="text-2xl ml-3 font-semibold ">Dashboard</text>
+            </div>
+            <button
+              className="bg-blue-800 hover:bg-blue-900 text-white py-3 px-6 cursor-pointer outline-none rounded-full"
+              onClick={connectWallet}
+            >
+              {connected ? "Disconnect wallet" : "Connect Wallet"}
+            </button>
           </div>
         </div>
+        <SideBar />
       </div>
       <Footer />
     </>
-  );
-}
-
-function DashboardNav() {
-  return (
-    <div>
-      <ul className="flex gap-6 ">
-        <li>Connect</li>
-        <li>Connect</li>
-        <li>Connect</li>
-      </ul>
-    </div>
   );
 }
