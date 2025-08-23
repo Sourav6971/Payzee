1ef1e59a94da200bbe59ef9675bdc8dc03742e9f
Sourav6971 <mailsourav014@gmail.com>
2025-08-23 09:23:23 +0530

    Cleaned the code and file structure

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ app.use("/api/account", require("./routes/account"));
 app.use("/api", require("./routes/transaction"));
 
 app.listen(PORT, () => {
-  console.log(`app listening on ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
--- server/solana/addAccount.js
+++
@@ -1,9 +0,0 @@
-const bs58 = require("bs58");
-const { Keypair } = require("@solana/web3.js");
-
-async function addAccount(privateKey) {
-  const keypair = await Keypair.fromSecretKey(bs58.default.decode(privateKey));
-  const publicAddress = keypair.publicKey.toBase58();
-  return publicAddress;
-}
-module.exports = { addAccount };
